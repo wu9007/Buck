fa8d1a82e0a1a18aac0ad9d38c844b5163121bf7
leyan95 <18231926271@163.com>
2020-02-27 13:56:43 +0800

    Formatter

--- lib/widgets/form/constant/formatter_constant.dart
+++ lib/widgets/form/constant/formatter_constant.dart
@@ -2,6 +2,16 @@ import 'package:flutter/services.dart';
 
 class Formatter {
   static final BlacklistingTextInputFormatter singleLineFormatter = BlacklistingTextInputFormatter(RegExp(r'\n'));
+
+  /// 整型
   static final WhitelistingTextInputFormatter digitsOnly = WhitelistingTextInputFormatter(RegExp(r'\d+'));
+
+  /// 浮点型
   static final WhitelistingTextInputFormatter floatOnly = WhitelistingTextInputFormatter(RegExp('[0-9.]'));
+
+  /// 浮点保留一位小数
+  static final WhitelistingTextInputFormatter floatReg1 = WhitelistingTextInputFormatter(RegExp(r'^(\d+)(.\d{0,1})?'));
+
+  /// 浮点保留两位小数
+  static final WhitelistingTextInputFormatter floatReg2 = WhitelistingTextInputFormatter(RegExp(r'^(\d+)(.\d{0,2})?'));
 }
